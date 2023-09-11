d2733704f111b59345493c6d15a2d5a7c8ce0232
HarshYadav <hy717330@gmail.com>
2023-09-11 19:56:18 +0530

    Authentication

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,9 @@ import 'package:projects/screens/diagnostics_screen.dart';
 import 'package:projects/screens/home_screen.dart';
 import 'package:projects/screens/registration_screens/login_screen.dart';
 import 'package:projects/screens/medication.dart';
+import 'package:projects/screens/registration_screens/otp_screen.dart';
 import 'package:projects/screens/tests_screen.dart';
+import 'package:projects/screens/user_detail_screen.dart';
 
 Future main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -19,14 +21,17 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Nirog',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(),
-      initialRoute: homeScreen,
+      home: const LoginScreen(),
+      initialRoute: loginScreen,
       routes: {
+          userDetails: (context)=> const UserDetails(),
+          otpScreen: (context)=> const OtpScreen(),
           loginScreen : (context) => const LoginScreen(),
           homeScreen : (context) => const MyHomePage(),
           diagnosticScreen : (context) => const Diagnostics(),
--- lib/screen_routes.dart
+++ lib/screen_routes.dart
@@ -1,5 +1,7 @@
 const String loginScreen = "login";
+const String otpScreen = "otp";
 const String homeScreen = "homescreen";
 const String diagnosticScreen = "diagScreen";
 const String testScreen = "testScreen";
 const String medicationScreen = "medicationScreen";
+const String userDetails = "userDetails";
