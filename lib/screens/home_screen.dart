eb2849b3d4248469004848c08fcbe2488a3a2201
Yuvraj <yuvrajsinghbhadoria@gmail.com>
2023-09-13 19:54:07 +0530

    HomeScreen Design

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -25,21 +25,69 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
         backgroundColor: Colors.lightBlue,
       ),
-      body: Center(
-        child: FutureBuilder(
-            future: context.read<HomeScreenProvider>().readUser(),
-            builder: (context, snapshot) {
-              if (snapshot.hasData) {
-                final user = snapshot.data;
-                return user == null
-                    ? Center(
-                        child: CircularProgressIndicator(),
-                      )
-                    : Text("Hi ${user.name.toString()}");
-              } else {
-                return SizedBox();
-              }
-            }),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            height: MediaQuery.of(context).size.height / 3,
+            color: Color(0xFF7CC4F8),
+            child: Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "Hi",
+                        style: TextStyle(
+                            fontSize: 30, fontWeight: FontWeight.bold),
+                      ),
+                      Text("Jai Kumar",
+                          style: TextStyle(
+                              fontSize: 30, fontWeight: FontWeight.bold)),
+                      SizedBox(
+                        height: 12,
+                      ),
+                      Text("4",
+                          style: TextStyle(
+                              fontSize: 30, fontWeight: FontWeight.bold)),
+                      Text("Reports",
+                          style: TextStyle(
+                              color: Color(0xFF8E8E8E),
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold)),
+                      Text("13",
+                          style: TextStyle(
+                              fontSize: 30, fontWeight: FontWeight.bold)),
+                      Text("Medication",
+                          style: TextStyle(
+                              color: Color(0xFF8E8E8E),
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold)),
+                    ],
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 30.0),
+                    child: Container(
+                      height: MediaQuery.of(context).size.height/2.5,
+                      width: MediaQuery.of(context).size.width/2.5,
+                      child: Image.asset('assets/images/profile.png'),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(
+              "Diagnosis",
+              style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+            ),
+          ),
+        ],
       ),
       drawer: const CustomNavigationDrawer(),
     );
